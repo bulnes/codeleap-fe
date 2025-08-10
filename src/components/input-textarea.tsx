320421e45b3503349e4bf1fcc677c5cd72196186
Bruno Bulnes <bulnes.bruno@gmail.com>
2025-08-10 11:52:20 -0300

    feat: add input-textarea component

---
+++ src/components/input-textarea.tsx
@@ -0,0 +1,23 @@
+import { forwardRef, type InputHTMLAttributes } from "react";
+
+interface InputTextAreaProps extends InputHTMLAttributes<HTMLTextAreaElement> {
+  label: string;
+}
+
+export const InputTextArea = forwardRef<
+  HTMLTextAreaElement,
+  InputTextAreaProps
+>(({ label, ...rest }, ref) => {
+  return (
+    <label className="block mb-4">
+      <span className="font-normal text-base mb-2 block">{label}</span>
+      <textarea
+        {...rest}
+        ref={ref}
+        className="w-full border border-[#777] rounded-lg py-2 px-3 h-20 resize-none"
+      />
+    </label>
+  );
+});
+
+InputTextArea.displayName = "InputTextArea";
