bbbc945cb138568bf5cc2b46ff25f8e037f0fba6
Bruno Bulnes <bulnes.bruno@gmail.com>
2025-08-10 11:52:29 -0300

    feat: add form component

---
+++ src/components/form.tsx
@@ -0,0 +1,32 @@
+import { InputText } from "./input-text";
+import { InputTextArea } from "./input-textarea";
+import { SubmitButton } from "./submit-button";
+import { Title } from "./title";
+
+export const Form = () => {
+  return (
+    <form className="p-6 rounded-2xl border border-[#999999]">
+      <Title>What’s on your mind?</Title>
+
+      <InputText
+        label="Title"
+        name="title"
+        id="title"
+        placeholder="Hello world"
+        required
+      />
+
+      <InputTextArea
+        label="Content"
+        name="content"
+        id="content"
+        placeholder="Content here"
+        required
+      />
+
+      <div className="text-right">
+        <SubmitButton>Create</SubmitButton>
+      </div>
+    </form>
+  );
+};
