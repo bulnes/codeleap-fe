e507b5e1b696e2993ba68c585c9d25b289571d2e
Bruno Bulnes <bulnes.bruno@gmail.com>
2025-08-10 14:51:11 -0300

    feat: add clsx to submit button

--- src/components/submit-button.tsx
+++ src/components/submit-button.tsx
@@ -1,18 +1,27 @@
+import clsx from "clsx";
 import { forwardRef } from "react";
 
 interface SubmitButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
+  buttonType?: "default" | "green" | "red";
 }
 
 export const SubmitButton = forwardRef<HTMLButtonElement, SubmitButtonProps>(
-  ({ children, ...rest }, ref) => {
+  ({ children, buttonType = "default", ...rest }, ref) => {
     return (
       <button
         {...rest}
         type="submit"
         ref={ref}
-        className="bg-codeleap-brand rounded-lg px-7 h-8 cursor-pointer disabled:opacity-50 disabled:cursor-not-allowed"
+        className={clsx(
+          "rounded-lg px-7 h-8 cursor-pointer disabled:opacity-50 disabled:cursor-not-allowed w-[120px]",
+          {
+            "bg-codeleap-brand": buttonType === "default",
+            "bg-[#FF5151]": buttonType === "red",
+            "bg-[#47B960]": buttonType === "green",
+          }
+        )}
       >
         <span className="font-bold text-white">{children}</span>
       </button>
